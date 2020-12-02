c5a1347ff072702d7142f595be638127523f7285
Mpho Khotleng <mkhotleng@2u.com>
2020-12-02 19:13:19 +0200

    welcome/landing page complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-let x = 'Site is Under Construction';
-// add emoji
+let x = '👩🏾‍💻🚧Site is Under Construction🚧👩🏾‍💻';
 alert(x);
 
-let h1Element = document.createElement('h2');
-let spanElement = document.createElement('span');
-spanElement.innerText = 'Welcome to my World!';
-h1Element.appendChild(spanElement);
-document.body.appendChild(h1Element);
+// let h1Element = document.createElement('h1');
+// let spanElement = document.createElement('span');
+// spanElement.innerText = 'Welcome to my World!';
+// h1Element.appendChild(spanElement);
+// document.body.appendChild(h1Element);
+
+document.getElementById('btnScrollTop')
+    .addEventListener('click', function(){
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    );
