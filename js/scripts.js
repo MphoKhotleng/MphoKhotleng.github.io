ca853ef7cd4e076a241dffb19143dcdc22812fc9
Mpho Khotleng <mkhotleng@2u.com>
2020-12-02 14:15:40 +0200

    re-work DOM manipulation

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let x = 'Site is Under Construction';
+// add emoji
+alert(x);
+
+let h1Element = document.createElement('h2');
+let spanElement = document.createElement('span');
+spanElement.innerText = 'Welcome to my World!';
+h1Element.appendChild(spanElement);
+document.body.appendChild(h1Element);
