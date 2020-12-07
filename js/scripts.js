79cd434c339eae7b484481dd7346a3c2669f0f14
Mpho Khotleng <mkhotleng@2u.com>
2020-12-07 16:08:05 +0200

    remove unused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,2 @@
 var x = '👩🏾‍💻🚧Site is Under Construction🚧👩🏾‍💻';
 alert(x);
-
-// let h1Element = document.createElement('h1');
-// let spanElement = document.createElement('span');
-// spanElement.innerText = 'Welcome to my World!';
-// h1Element.appendChild(spanElement);
-// document.body.appendChild(h1Element);
-
-// document.getElementById('btnScrollTop')
-//     .addEventListener('click', function(){
-
-//         window.scrollTo({
-//             top: 0,
-//             left: 0,
-//             behavior: 'smooth'
-//         });
-//     }
-
-//     );
